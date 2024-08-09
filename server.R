891ad0783d42675bc566f36ec990565317f8f7cd
superuserkalo <gantchevkaloyan@proton.me>
2024-08-09 15:24:55 +0200

    +1

--- server.R
+++ server.R
@@ -13,46 +13,57 @@ source("utils.R")
 
 # Python part
 use_python("/opt/homebrew/bin/python3", required = TRUE)
+dict <- reticulate::dict
 oracledb <- import("oracledb")
 pd <- import("pandas")
 
-account_creation <- function(username, password, DB_username, DB_password, DB_dsn){
+account_creation <- function(username, password){
+    conn <- oracledb$connect(user = DB_username, password = DB_password, dsn = DB_dsn)
+    cursor <- conn$cursor()
+    
+    # Check if username already exists
+    cursor$execute("SELECT COUNT(*) as count FROM mtg_users WHERE USERNAME = :1",list(username))
+    result <- as.integer(cursor$fetchone()[[1]])
+    
+    if (result > 0) {
+      shinyalert("Account already exists", "The account for this username already exists.", type = "error")
+    } else {
+      # Hash username and password
+      username_hash <- sodium::password_store(username)
+      password_hash <- sodium::password_store(password)
+      
+      username_hash_hex <- sodium::bin2hex(as.raw(username_hash))
+      password_hash_hex <- sodium::bin2hex(as.raw(password_hash))
+      
+      # Insert new user
+      cursor$execute(
+        "INSERT INTO mtg_users (username, username_hash, password_hash) VALUES (:1, :2, :3)", 
+        list(username, username_hash_hex, password_hash_hex)
+      )
+      conn$commit()
+      shinyalert("Account created", "The account has been successfully created.", type = "success")  
+    }
+  cursor$close()
+  conn$close()
+}
+
+login <- function(username, password){
   conn <- oracledb$connect(user = DB_username, password = DB_password, dsn = DB_dsn)
   cursor <- conn$cursor()
   
-  query <- "SELECT COUNT(*) as count FROM mtg_users WHERE username = ?"
-  cursor$execute(query, list(username = username))
-  result <- cursor$fetchone()
-  
-  if (result$count > 0) {
-    shinyalert("Account already exists", "The account for this username already exists.", type = "error")
-  } else {
-    username_hash <- sodium::password_store(username)
-    password_hash <- sodium::password_store(password)
-    
-    dbExecute(conn, glue("INSERT INTO mtg_users (username, username_hash, password_hash) VALUES (?, ?, ?)"),
-              params = list(username, username_hash, password_hash))
-    
-    shinyalert("Account created", "The account has been successfully created.", type = "success")  
-  }
-  dbDisconnect(conn)
-}
-
-login <- function(username, password,DB_username,DB_password,DB_dsn){
-  conn <- oracledb$connect(user = DBusername, DBpassword = DBpassword, dsn = DBdsn)
-  
   # Retrieve the stored hashes
-  query <- glue("SELECT username_hash, password_hash FROM mtg_users WHERE username = ?")
-  result <- dbGetQuery(conn, query, params = list(username))
-  dbDisconnect(conn)
+  cursor$execute(glue("SELECT USERNAME_HASH, PASSWORD_HASH FROM mtg_users WHERE USERNAME = :1"), list(username))
+  result <- cursor$fetchone()
+  cursor$close()
+  conn$close()
   
-  if (nrow(result) == 0) {
+  if (length(result) == 0) {
     shinyalert("Login failed!", "The account for this username doesn't exist", type = "error")
     return(FALSE)
   }
   
   stored_username_hash <- result$username_hash[[1]]
-  stored_password_hash <- result$password_hash[[1]]
+  stored_password_hash <- result$password_hash[[2]]
   
   # Verify username and password
   username_verified <- sodium::password_verify(stored_username_hash, username)
@@ -83,7 +94,7 @@ server <- function(input, output, session) {
     password <- input$passInput
     
     if (empty_fields(username,password)) {
-      account_creation(username,password,DB_username,DB_password,DB_dsn)    
+      account_creation(username,password)    
       
       # Clears the text inputs after successful login
       updateTextInput(session, "usernameInput", value = "")
@@ -98,7 +109,7 @@ server <- function(input, output, session) {
     password <- input$passInput
     
     if (empty_fields(username,password)) {
-      login_successful <- login(username,password,DB_username,DB_password,DB_dsn)
+      login_successful <- login(username,password)
       if (login_successful){
         shinyalert("Success", "Login successful!", type = "success")
         runjs('$("#login_screen").hide(); $("#main_page").show();')
